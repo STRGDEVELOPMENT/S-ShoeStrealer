f144c1bd6bb71ace48c2309d9f88a00d9e81eee3
STRG <134840814+STRGDEVELOPMENT@users.noreply.github.com>
2023-05-28 22:09:04 +0100

    Add files via upload

---
+++ S-ShoeStealer/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'A Simple Standalone Shoe Stealing Script'
+author 'https://forum.cfx.re/u/strg/summary'
+version '1.0.0'
+
+client_script '**/cl_*.lua'
+server_script 'sv_main.lua'
+lua54 'yes'
